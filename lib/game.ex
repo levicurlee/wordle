1decba64f5d4506164630d55d964178d552168a2
levicurlee <levicurlee@gmail.com>
2026-03-31 15:21:36 -0400

    added logic for checking greys, greens, and yellows

--- lib/game.ex
+++ lib/game.ex
@@ -18,11 +18,45 @@ defmodule Wordle.Game do
     %{game | guesses: game.guesses ++ [guess]}
   end
 
-  def count_greens(game, guess) do
+  def mark_characters(game, guess) do
     game.answer
     |> String.graphemes()
     |> Enum.zip(String.graphemes(guess))
-    |> Enum.count(fn {a, g} -> a == g end)
+    |> Enum.map(&compare_greens/1)
+    |> mark_yellows()
   end
 
+  def compare_greens({answer, guess}) do
+    color = if answer == guess, do: :green, else: :gray
+    {answer, guess, color}
+  end
+
+  def mark_yellows(marks) do
+    yellow_or_gray =
+      marks
+      |> Enum.filter(fn {_, _, color} -> color == :gray end)
+
+    answers = Enum.map(yellow_or_gray, fn {a, _g, _color} -> a end)
+
+    guesses = Enum.map(yellow_or_gray, fn {_, g, _color} -> g end)
+
+    greys = guesses -- answers
+
+    mark_rest(marks, greys, [])
+  end
+
+  def mark_rest([], _greys, acc), do: Enum.reverse(acc)
+
+  def mark_rest([{a, g, color} | marks], greys, acc) do
+    cond do
+      color == :green ->
+        mark_rest(marks, greys, [{a, g, :green} | acc])
+
+      g in greys ->
+        mark_rest(marks, List.delete(greys, g), [{a, g, :grey} | acc])
+
+      true ->
+        mark_rest(marks, greys, [{a, g, :yellow} | acc])
+    end
+  end
 end
